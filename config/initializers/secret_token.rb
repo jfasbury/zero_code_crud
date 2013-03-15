17555fb64d33b0b17a06233923cda9024823f011
Joey Asbury <joey.asbury@gmail.com>
2013-03-14 20:54:46 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZeroCodeCrud::Application.config.secret_token = '58c78ab61d776f408fed1b367d6d52f2535d0cc623130d1267ec492b0cb2de7d9f86309c58e78f602f6bc31304babc43555c2bee5e2ddb64895acf5dbf26cb57'
